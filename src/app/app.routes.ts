7e43cbeacfa4fa552c54bda1e3103120d21669be
Francisco Perez <117410205+FranSfw@users.noreply.github.com>
2025-10-17 15:13:02 -0700

    FirstCommit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Navbar } from './components/navbar/navbar';
+import { Mainsito } from './views/mainsito/mainsito';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'navbar', component: Navbar},
+    { path: '', component: Mainsito},
+];
---
+++ src/app/views/mainsito/mainsito.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mainsito',
+  imports: [],
+  templateUrl: './mainsito.html',
+  styles: ``
+})
+export class Mainsito {
+
+}
